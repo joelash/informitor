5976c5cd0b8b5fd9785f5c522c5599bdb69cb897
Joel Friedman <asher.friedman@gmail.com>
2011-08-19 07:43:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Informitor::Application.config.secret_token = 'db6a8c338e467e4a58e2f6e7a87cfc570fa7ac67be045ce943b5893105b73df9bc99231000affb10d1a5d3be970e6a098eaae573cca60cdd69fd897f8f47fb89'
